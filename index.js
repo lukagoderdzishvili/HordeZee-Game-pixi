bf44eed50b11c615902dbe9b8bd4cbb3c342fc34
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-01-22 15:24:12 +0400

    Add enemy

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
+import Victor from "victor";
 //import Matter from "matter-js";
 
 let canvasSize = 256;
@@ -20,6 +20,16 @@ square.tint = 0xea985d;
 
 app.stage.addChild(square);
 
+let enemyRadius = 16;
+const enemy = new PIXI.Graphics();
+let r = randomSpawnPoint();
+enemy.position.set(r.x, r.y);
+enemy.beginFill(0xff0000, 1);
+enemy.drawCircle(0, 0, enemyRadius);
+enemy.endFill();
+app.stage.addChild(enemy);
+
+
 
 app.ticker.add((delta) => {
     const cursorPosition = app.renderer.plugins.interaction.mouse.global;
@@ -31,3 +41,26 @@ app.ticker.add((delta) => {
         Math.PI / 2;
     square.rotation = angle;
 });
+
+function randomSpawnPoint(){
+    let edge = Math.floor(Math.random() * 4); //random int between 0 and 3
+    let spawnPoint = new Victor(0, 0);
+    switch(edge){
+        case 0: //TOP
+            spawnPoint.x = canvasSize  * Math.random();
+            break;
+        case 1: //RIGHT
+            spawnPoint.x = canvasSize;
+            spawnPoint.y = canvasSize * Math.random();
+            break;
+        case 2: //BOTTOM
+            spawnPoint.x = canvasSize * Math.random();
+            spawnPoint.y = canvasSize;
+            break;
+        default: //LEFT
+            spawnPoint.x = 0;
+            spawnPoint.y = canvasSize * Math.random();
+            break;
+    }
+    return spawnPoint;
+}
